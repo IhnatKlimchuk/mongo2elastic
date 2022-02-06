caafea951ca5d2c4b77b1c3f49385467a2e7b7bf
Ihnat Klimchuk <ihnat.klimchuk@gmail.com>
2022-02-06 22:15:00 +0300

    Add mongo replication repository

---
+++ src/Notidar.Mongo2Elastic/Replication/MongoReplicationStateRepository.cs
@@ -0,0 +1,59 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Notidar.Mongo2Elastic.Replication
+{
+    public class MongoReplicationStateRepository : IReplicationStateRepository
+    {
+        private readonly IMongoCollection<ReplicationState> _stateCollection;
+
+        public MongoReplicationStateRepository(IMongoCollection<ReplicationState> stateCollection)
+        {
+            _stateCollection = stateCollection ?? throw new ArgumentNullException(nameof(stateCollection));
+        }
+
+        public Task<ReplicationState?> TryLockStateAsync(
+            string replicationKey,
+            Guid replicatorId,
+            DateTime lockExpirationDateUtc,
+            CancellationToken cancellationToken = default)
+        {
+            var utcNow = DateTime.UtcNow;
+            return _stateCollection
+                .FindOneAndUpdateAsync(
+                    filter: Builders<ReplicationState>.Filter.And(
+                        Builders<ReplicationState>.Filter.Eq(document => document.ReplicationKey, replicationKey),
+                        Builders<ReplicationState>.Filter.Or(
+                            Builders<ReplicationState>.Filter.Eq(document => document.LockExpirationDateUtc, BsonNull.Value.ToNullableUniversalTime()),
+                            Builders<ReplicationState>.Filter.Lte(document => document.LockExpirationDateUtc, utcNow),
+                            Builders<ReplicationState>.Filter.Eq(document => document.ReplicatorId, BsonNull.Value.AsNullableGuid),
+                            Builders<ReplicationState>.Filter.Eq(document => document.ReplicatorId, replicatorId))),
+                    update: Builders<ReplicationState>.Update.Combine(
+                        Builders<ReplicationState>.Update.Set(document => document.ReplicatorId, replicatorId),
+                        Builders<ReplicationState>.Update.Set(document => document.LockExpirationDateUtc, lockExpirationDateUtc),
+                        Builders<ReplicationState>.Update.Set(document => document.UpdatedAtUtc, DateTime.UtcNow)),
+                    options: new FindOneAndUpdateOptions<ReplicationState> { IsUpsert = false, ReturnDocument = ReturnDocument.After },
+                    cancellationToken: cancellationToken);
+        }
+
+        public Task<ReplicationState?> TryUpdateStateAsync(
+            string replicationKey,
+            Guid replicatorId,
+            DateTime lockExpirationDateUtc,
+            BsonDocument? resumeToken = null,
+            CancellationToken cancellationToken = default)
+        {
+            return _stateCollection
+                .FindOneAndUpdateAsync(
+                    filter: Builders<ReplicationState>.Filter.And(
+                        Builders<ReplicationState>.Filter.Eq(document => document.ReplicationKey, replicationKey),
+                        Builders<ReplicationState>.Filter.Eq(document => document.ReplicatorId, replicatorId)),
+                    update: Builders<ReplicationState>.Update.Combine(
+                        Builders<ReplicationState>.Update.Set(document => document.LockExpirationDateUtc, lockExpirationDateUtc),
+                        Builders<ReplicationState>.Update.Set(document => document.ResumeToken, resumeToken),
+                        Builders<ReplicationState>.Update.Set(document => document.UpdatedAtUtc, DateTime.UtcNow)),
+                    options: new FindOneAndUpdateOptions<ReplicationState> { IsUpsert = false, ReturnDocument = ReturnDocument.After },
+                    cancellationToken: cancellationToken);
+        }
+    }
+}
