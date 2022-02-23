8bd6d0aa7989cf79112648a8c3d19b46ad8fed7f
Ihnat Klimchuk <ihnat.klimchuk@gmail.com>
2022-02-24 01:05:49 +0300

    Update ReplicatorBuilderExtensions.cs

--- src/Notidar.Mongo2Elastic.MongoDb/Builder/ReplicatorBuilderExtensions.cs
+++ src/Notidar.Mongo2Elastic.MongoDb/Builder/ReplicatorBuilderExtensions.cs
@@ -1,18 +1,21 @@
 ﻿using MongoDB.Driver;
 using Notidar.Mongo2Elastic.Builder;
+using System.Linq.Expressions;
 
 namespace Notidar.Mongo2Elastic.MongoDB.Builder
 {
     public static class ReplicatorBuilderExtensions
     {
         public static IReplicatorBuilder<TSource, TDestination> FromMongoDb<TSource, TDestination>(
-            this IReplicatorBuilder<TSource, TDestination> replicatorBuilder, IMongoCollection<TSource> mongoCollection, Action<SourceRepositoryOptions> configureAction = null)
+            this IReplicatorBuilder<TSource, TDestination> replicatorBuilder, IMongoCollection<TSource> mongoCollection,
+            Action<SourceRepositoryOptions> configureAction = null,
+            params Expression<Func<TSource, object>>[] projectionExcludeFields)
             where TSource : class
             where TDestination : class
         {
             var options = new SourceRepositoryOptions();
             configureAction?.Invoke(options);
-            return replicatorBuilder.Add(new SourceRepository<TSource>(mongoCollection, options));
+            return replicatorBuilder.Add(new SourceRepository<TSource>(mongoCollection, options, projectionExcludeFields));
         }
 
         public static IReplicatorBuilder<TSource, TDestination> WithMongoDbState<TSource, TDestination>(
